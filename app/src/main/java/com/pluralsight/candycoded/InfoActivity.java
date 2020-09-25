1c4388568f6b85b0fc7fd462afd713b30a540cba
ohassine <oussamahassine93@gmail.com>
2020-09-25 12:28:01 +0100

    Completed module task 3

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -36,8 +36,11 @@ public class InfoActivity extends AppCompatActivity {
             startActivity(mapIntent);
     }
 
-    // ***
-    // TODO - Task 3 - Launch the Phone Activity
-    // ***
+    public void createPhoneIntent(View view){
+        Uri uri = Uri.parse("tel:0123456789");
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(uri);
+        startActivity(intent);
+    }
 }
 
